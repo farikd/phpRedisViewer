99ebeec1dc65936dfb238996dbf05a60fb9eddbc
farik <farikd@gmail.com>
2013-10-18 14:38:09 +0300

    save state

--- includes/rv.js
+++ includes/rv.js
@@ -2,42 +2,47 @@ rv = {
 
     tree: null,
     storage: null,
+    state: null,
 
     init: function () {
         rv.storage = $.localStorage;
         rv.tree = $('#tree')
+
+
+        rv.tree.bind('tree.init', function () {
+            console.log('tree init');
+        });
+        rv.tree.bind('tree.click', rv.nodeClick);
+        rv.tree.bind('tree.select', rv.nodeSelect);
+        rv.tree.bind('tree.open', rv.changeState);
+        rv.tree.bind('tree.close', rv.changeState);
+
+        var ms = new Date().getTime();
         rv.tree.tree({
-            data: '/r.php?m=tree',
-            selectable: false,
+            data: '/r.php?m=tree&ms=' + ms,
+            selectable: true,
             slide: false,
             useContextMenu: false
         });
 
-        rv.treeRefresh();
 
-        $('#tree').bind('tree.click', rv.nodeClick);
+        rv.treeRefresh();
     },
 
     treeRefresh: function () {
-        rv.tree.tree('loadDataFromUrl', '/r.php?m=tree');
-        var state = rv.storage.get('state');
+        var ms = new Date().getTime();
+        rv.tree.tree('loadDataFromUrl', '/r.php?m=tree&ms=' + ms, null, rv.setState);
+    },
+
+    getState: function () {
+        var state = rv.tree.tree('getState');
+        rv.storage.set('state', state);
         console.log(state);
-        //rv.tree.tree('setState', state);
+    },
 
-        if (state && state.open_nodes && state.open_nodes.length > 0) {
-
-            $.each(state.open_nodes, function (index, id) {
-                var node = rv.tree.tree('getNodeById', id);
-                if (node) {
-                    rv.tree.tree('openNode', node);
-                    console.log('node founded ' + id);
-                    console.log(typeof node);
-                    rv.tree.tree('openNode', id);
-                } else {
-                    console.log('node not founded ' + id);
-                }
-            });
-        }
+    setState: function () {
+        rv.tree.tree('setState', rv.storage.get('state'));
+        
     },
 
     load: function (key) {
@@ -45,25 +50,42 @@ rv = {
         $('#data h1').text(key);
         var ms = new Date().getTime();
         $.post('/r.php?m=load&ms=' + ms, {key: key}, function (data) {
-            console.log(data);
             $('#data .data').html(data.data);
         })
     },
 
     nodeClick: function (event) {
-
-        var state = rv.tree.tree('getState');
-        rv.storage.set('state', state);
-        console.log(state);
-
         var node = event.node;
+        rv.tree.tree('selectNode', node);
         if (node.children.length > 0) {
+            rv.tree.tree('toggle', node);
+        }
+        return false;
 
 
+        if (node.children.length > 0) {
             rv.tree.tree('toggle', node);
         } else if (node.fullpath) {
-            rv.load(node.fullpath)
+            rv.tree.tree('selectNode', node);
+            rv.load(node.fullpath);
+        }
+        rv.getState();
+    },
+
+    nodeSelect: function (e) {
+        console.log('select');
+        if (e.node) {
+            console.log('node selected');
+            if (e.node.children.length == 0) {
+                console.log('no children');
+                rv.load(e.node.fullpath);
+                rv.changeState();
+            }
         }
 
+    },
+
+    changeState: function () {
+        rv.getState();
     }
 }
